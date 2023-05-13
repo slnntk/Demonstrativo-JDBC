d5d3054c81a42fa9320e914b09c8913d4d8e45b3
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-13 11:09:22 -0300

    DB class create and implemented

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,16 +1,66 @@
 package db;
 
 import java.io.FileInputStream;
-import java.sql.Connection;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
 
     private static Connection conn;
 
-    private static Properties loadProperties(){
-        try (FileInputStream fs = new)
 
+    public static Connection getConnection(){
+        if (conn == null){
+            try {
+                Properties ps = loadProperties();
+                String dburl = ps.getProperty("dburl");
+                conn = DriverManager.getConnection(dburl, ps);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return conn;
+    }
+
+    private static Properties loadProperties(){
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties ps = new Properties();
+            ps.load(fs);
+            return ps;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(){
+        if (conn != null){
+            try{
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st){
+        if (st != null){
+            try{
+                st.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public static void closeConnection(ResultSet rs){
+        if (rs != null){
+            try{
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
