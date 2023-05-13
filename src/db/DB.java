a6c43cd884196c410c1cc2e2432f77d66ba8887a
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-13 11:05:21 -0300

    first commit

---
+++ src/db/DB.java
@@ -0,0 +1,16 @@
+package db;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn;
+
+    private static Properties loadProperties(){
+        try (FileInputStream fs = new)
+
+    }
+
+}
